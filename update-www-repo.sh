d3ad4e16e3cebc669a3a1b0405cff45d8fff41fc
Eric Prud'hommeaux <eric+github@w3.org>
2020-04-23 10:30:51 +0200

    ~ use mocha-slow-options to quiet slow (> 75ms) messages

--- update-www-repo.sh
+++ update-www-repo.sh
@@ -4,6 +4,6 @@
     echo "\n\`\`\`" >> README-new.md &&
     (cd www && tree .) >> README-new.md &&
     echo "\`\`\`\n\n\n## test run output\n\n\`\`\`" >> README-new.md &&
-    npm test >> README-new.md &&
+    npm run test:quiet >> README-new.md &&
     echo "\`\`\`" >> README-new.md &&
     mv README-new.md www/README.md
