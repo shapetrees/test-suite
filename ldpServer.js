f38e0262a464c89f4f41862888fcf9051542acc4
Eric Prud'hommeaux <eric+github@w3.org>
2020-05-06 19:15:05 +0200

    ~ mv test-suite/ecosystems shapetree.js/ecosystems

--- ldpServer.js
+++ ldpServer.js
@@ -21,7 +21,7 @@ const Errors = require('./shapetree.js/lib/rdf-errors');
 const FileSystem = new (require('./filesystems/fs-promises-utf8'))(LdpConf.documentRoot, LdpConf.indexFile, RdfSerialization)
 const CallEcosystemFetch = (url, /* istanbul ignore next */options = {}) => Ecosystem.fetch(url, options); // avoid circular dependency on ShapeTree and Ecosystem.
 const ShapeTree = require('./shapetree.js/lib/shape-tree')(FileSystem, RdfSerialization, require('./filesystems/fetch-self-signed')(CallEcosystemFetch))
-const Ecosystem = new (require('./ecosystems/simple-apps'))(FileSystem, ShapeTree, RdfSerialization);
+const Ecosystem = new (require('./shapetree.js/ecosystems/simple-apps'))(FileSystem, ShapeTree, RdfSerialization);
 
 // Prepare server
 const ldpServer = Express();
