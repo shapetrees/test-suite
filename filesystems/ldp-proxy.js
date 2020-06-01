6e15d796d87c7908fc0a5af2455d1207ee20fe05
Eric Prud'hommeaux <eric+github@w3.org>
2020-06-01 08:43:34 +0200

    + ShapeTress as client API calls

--- filesystems/ldp-proxy.js
+++ filesystems/ldp-proxy.js
@@ -1,5 +1,7 @@
 const Fs = require('fs');
 const Path = require('path');
+const Log = require('debug')('								ldp-proxy');
+const Details = Log.extend('details');
 const Prefixes = require('../shapetree.js/lib/prefixes');
 const { DataFactory } = require("n3");
 const { namedNode, literal, defaultGraph, quad } = DataFactory;
@@ -34,6 +36,7 @@ class ldpProxy {
    *     rstat(myUrl).then(stat => true, e => false)
    */
   async rstat (url) {
+    Details('rstat(<%s>)', url.pathname);
     const resp = await this.fetch(new URL(url, this.ldpServer));
     if (resp.status !== 200) {
       // Simulate a require('fs').lstat('doesNotExist') error.
@@ -62,6 +65,7 @@ class ldpProxy {
    * @throws: resource does not exist
    */
   async read (url) {
+    Details('read(<%s>)', url.pathname);
     return this.fetch(new URL(url, this.ldpServer));
   }
 
@@ -70,6 +74,7 @@ class ldpProxy {
    * @throws: resource does not exist
    */
   async write (url, body) {
+    Details('write(<%s>, "%s...")', url.pathname, body.substr(0, 60).replace(/\n/g, '\\n'));
     return this.fetch(new URL(url, this.ldpServer), {
       method: 'PUT',
       headers: {'content-type': 'text/turtle'},
@@ -85,6 +90,7 @@ class ldpProxy {
    * @returns: [newly-minuted name, undefined]
    */
   async invent (parentUrl, requestedName, body, mediaType) {
+    Details('invent(<%s>, "%s", %d characters, "")', parentUrl.pathname, requestedName, body.length, mediaType);
     const resp = await this.fetch(parentUrl, {
       method: 'POST',
       headers: {
@@ -111,6 +117,7 @@ class ldpProxy {
    * @throws: resource does not exist
    */
   async remove (url) {
+    Details('remove(<%s>)', url.pathname);
     return this.fetch(new URL(url, this.ldpServer), {
       method: 'DELETE'
     });
@@ -126,6 +133,7 @@ class ldpProxy {
    *   parser failures
    */
   async readContainer (url, prefixes) {
+    Details('readContainer(<%s>, %s)', url.pathname, JSON.stringify(prefixes))
     const resp = await this.fetch(new URL(url, this.ldpServer));
     const text = await resp.text();
     return this._rdfInterface.parseTurtle(text, url, prefixes);
@@ -139,6 +147,7 @@ class ldpProxy {
    *   serializer failures
    */
   async writeContainer (url, graph, prefixes) {
+    Details('writeContainer(<%s>, Store() with %d quads, %s)', url.pathname, graph.size, JSON.stringify(prefixes))
     const body = await this._rdfInterface.serializeTurtle(graph, url, prefixes);
     return await this.fetch(new URL(url, this.ldpServer), {
       method: 'PUT',
@@ -156,6 +165,7 @@ class ldpProxy {
    * @returns: [newly-minuted URL, Container graph]
    */
   async inventContainer (parentUrl, requestedName, title, prefixes = {}) {
+    Details('inventContainer(<%s>, "%s", "${title}", %s)', parentUrl.pathname, requestedName, JSON.stringify(prefixes));
     const reqOpts = {
       method: 'POST',
       headers: {
@@ -193,6 +203,7 @@ class ldpProxy {
    * @throws: resource does not exist
    */
   async removeContainer (url) {
+    Details('remove(<%s>)', url.pathname);
     const path = new URL(url, this.ldpServer);
     const graph = await this.readContainer(path, {});
     const children = graph
@@ -218,6 +229,7 @@ class ldpProxy {
    *   serializer failures
    */
   async ensureContainer (url, prefixes, title) {
+    const funcDetails = Details.extend(`ensureContainer(<${url.pathname}>, ${JSON.stringify(prefixes)}, "title")`);
     const _ldpProxy = this;
     const dummy = new URL('.DUMMY', new URL(url, this.ldpServer));
     await this.fetch(dummy, {
