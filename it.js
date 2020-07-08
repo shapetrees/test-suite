fccd6a69910abbf4e1ae914b71a73ce9cec456ff
Eric Prud'hommeaux <eric+github@w3.org>
2020-07-08 14:31:46 +0200

    + ShapeTree.cache

--- it.js
+++ it.js
@@ -12,7 +12,7 @@ const Ns = {
 }
 
 const UseHTTP = false
-const [Base, Fetch, NodeDocumentLoader] = UseHTTP
+const [Base, Fetch, LdLibraryDocumentLoader] = UseHTTP
       ? [
         new URL('http://localhost/checkouts/shapetrees/test-suite/'),
         require('node-fetch'),
@@ -45,12 +45,21 @@ class ShapeTree {
     async function load () {
       const resp = await Fetch(this.url)
       const text = await resp.text()
-      const tree = await parseShapeTree(this.url, text)
-      // const tree = JSON.parse(text)
-      console.warn(JSON.stringify(tree, null, 2))
+      // const tree = JSON.parse(text) // if it's JSON, we can shortcut the graph parser
+      const tree = await ShapeTree.parse(this.url, text)
       this.indexStep(tree)
       return this
     }
+  }
+
+  static async get (url) {
+    const cachename = noHash(url)
+    if (ShapeTree.cache && cachename in ShapeTree.cache)
+      return ShapeTree.cache[cachename]
+    const ret = await new ShapeTree(url).ready
+    if (ShapeTree.cache)
+      ShapeTree.cache[cachename] = ret
+    return ret
   }
 
   indexStep (step) {
@@ -76,7 +85,7 @@ class ShapeTree {
         if (urlMatch) {
           // parse a new ShapeTree
           const [undefined, rel, fragment] = urlMatch
-          const t = await new ShapeTree(new URL(rel, _ShapeTree.url)).ready
+          const t = await ShapeTree.get(new URL(rel, _ShapeTree.url))
           const it = t.walkReferences(fragment, via.concat(step))
 
           // walk its iterator responses
@@ -90,10 +99,103 @@ class ShapeTree {
       }
     }
   }
+
+  static async parse (node, text) {
+    const g = new N3.Store()
+    const p1 = new Promise((acc, rej) => {
+      setTimeout(() => acc(5), 200)
+    })
+    g.addQuads(await Jsonld.toRDF(JSON.parse(text), {
+      // format: "application/nquads",
+      base: node.href,
+      documentLoader: async (url, callback) => {
+        if (UseHTTP)
+          return LdLibraryDocumentLoader(url, callback)
+        if (ShapeTree.cache && url in ShapeTree.cache)
+          return callback(null, { contextUrl: null, document: JSON.parse(ShapeTree.cache[url]), documentUrl: url })
+        const resp = await Fetch(new URL(url))
+        const text = await resp.text()
+        if (ShapeTree.cache)
+          ShapeTree.cache[url] = text // !! heterogeneous cache
+        callback(null, { contextUrl: null, document: JSON.parse(text), documentUrl: url })
+      }
+    }))
+    const contents = namedNode(Ns.tree + 'contents')
+    const root = g.getQuads(null, contents, null).find(
+      q => g.getQuads(null, contents, q.subject).length === 0
+    ).subject
+    const docUrl = noHash(node)
+
+    const str = sz => one(sz).value
+    const sht = sz => shorten(one(sz).value)
+    const cnt = sz => visitStep(stepRules, sz, true)
+    const ref = sz => visitStep(referenceRules, sz, false)
+    const one = sz => sz.length === 1
+          ? sz[0]
+          : (() => {throw Error(`Expected one object, got [${sz.map(s => s.value).join(', ')}]`)})()
+    const referenceRules = [
+      { predicate: Ns.tree + 'treeStep'    , attr: 'treeStep'    , f: sht },
+      { predicate: Ns.tree + 'shapePath'   , attr: 'shapePath'   , f: str },
+    ]
+    const stepRules = [
+      { predicate: Ns.tree + 'expectedType', attr: 'expectedType', f: sht },
+      { predicate: Ns.rdfs + 'label'       , attr: 'name'        , f: str },
+      { predicate: Ns.tree + 'uriTemplate' , attr: 'uriTemplate' , f: str },
+      { predicate: Ns.tree + 'shape'       , attr: 'shape'       , f: sht },
+      { predicate: Ns.tree + 'contents'    , attr: 'contents'    , f: cnt },
+      { predicate: Ns.tree + 'references'  , attr: 'references'  , f: ref },
+    ]
+
+    const ret = visitStep(stepRules, [root], true)[0]
+    return Object.assign({"@context": "../ns/shapeTreeContext"}, ret)
+
+    function visitStep (rules, subjects, includeId) {
+      return subjects.map(s => {
+        const byPredicate = g.getQuads(s, null, null).reduce((acc, q) => {
+          const p = q.predicate.value
+          if (!(p in acc))
+            acc[p] = []
+          acc[p].push(q.object)
+          return acc
+        }, {})
+        const ret = includeId
+              ? {'@id': shorten(s.value)}
+              : {}
+        return rules.reduce((acc, rule) => {
+          const p = rule.predicate
+          if (p in byPredicate)
+            acc[rule.attr] = rule.f(byPredicate[p])
+          return acc
+        }, ret)
+      })
+    }
+
+    function shorten (urlStr) {
+      for (const prefix in Ns) {
+        const ns = Ns[prefix]
+        if (urlStr.startsWith(ns)) {
+          const localname = urlStr.substr(ns.length)
+          if (localname.match(/^[a-zA-Z]+$/))
+            return prefix === ''
+            ? localname
+            : prefix + ':' + localname
+        }
+      }
+      if (new URL(node.href).host === new URL(urlStr).host) // https://github.com/stevenvachon/relateurl/issues/28
+        return Relateurl.relate(node.href, urlStr, { output: Relateurl.ROOT_PATH_RELATIVE })
+      return urlStr
+    }
+  }
+
+}
+
+function noHash (url) {
+  const u = url.href
+  return new URL(u.substr(0, u.length - url.hash.length))
 }
 
 async function test (from) {
-  const st = await new ShapeTree(from).ready
+  const st = await ShapeTree.get(from)
   const it = st.walkReferences('#org')
   const result = []
 
@@ -109,99 +211,12 @@ async function test (from) {
   return { from, result }
 }
 
-async function parseShapeTree (node, text) {
-  const g = new N3.Store()
-  const p1 = new Promise((acc, rej) => {
-    setTimeout(() => acc(5), 200)
-  })
-  g.addQuads(await Jsonld.toRDF(JSON.parse(text), {
-    // format: "application/nquads",
-    base: node.href,
-    documentLoader: async (url, callback) => {
-      if (UseHTTP)
-        return NodeDocumentLoader(url, callback)
-      const resp = await Fetch(new URL(url))
-      const text = await resp.text()
-      callback(null, { contextUrl: null, document: JSON.parse(text), documentUrl: url })
-    }
-  }))
-  const contents = namedNode(Ns.tree + 'contents')
-  const root = g.getQuads(null, contents, null).find(
-    q => g.getQuads(null, contents, q.subject).length === 0
-  ).subject
-  const docUrl = noHash(node)
-  console.warn(docUrl.href, root)
-
-  const str = sz => one(sz).value
-  const sht = sz => shorten(one(sz).value)
-  const cnt = sz => visitStep(stepRules, sz, true)
-  const ref = sz => visitStep(referenceRules, sz, false)
-  const one = sz => sz.length === 1
-        ? sz[0]
-        : (() => {throw Error(`Expected one object, got [${sz.map(s => s.value).join(', ')}]`)})()
-  const referenceRules = [
-    { predicate: Ns.tree + 'treeStep'    , attr: 'treeStep'    , f: sht },
-    { predicate: Ns.tree + 'shapePath'   , attr: 'shapePath'   , f: str },
-  ]
-  const stepRules = [
-    { predicate: Ns.tree + 'expectedType', attr: 'expectedType', f: sht },
-    { predicate: Ns.rdfs + 'label'       , attr: 'name'        , f: str },
-    { predicate: Ns.tree + 'uriTemplate' , attr: 'uriTemplate' , f: str },
-    { predicate: Ns.tree + 'shape'       , attr: 'shape'       , f: sht },
-    { predicate: Ns.tree + 'contents'    , attr: 'contents'    , f: cnt },
-    { predicate: Ns.tree + 'references'  , attr: 'references'  , f: ref },
-  ]
-
-  const ret = visitStep(stepRules, [root], true)[0]
-  return Object.assign({"@context": "../ns/shapeTreeContext"}, ret)
-
-  function visitStep (rules, subjects, includeId) {
-    return subjects.map(s => {
-      const byPredicate = g.getQuads(s, null, null).reduce((acc, q) => {
-        const p = q.predicate.value
-        if (!(p in acc))
-          acc[p] = []
-        acc[p].push(q.object)
-        return acc
-      }, {})
-      const ret = includeId
-            ? {'@id': shorten(s.value)}
-            : {}
-      return rules.reduce((acc, rule) => {
-        const p = rule.predicate
-        if (p in byPredicate)
-          acc[rule.attr] = rule.f(byPredicate[p])
-        return acc
-      }, ret)
-    })
-  }
-
-  function shorten (urlStr) {
-    for (const prefix in Ns) {
-      const ns = Ns[prefix]
-      if (urlStr.startsWith(ns)) {
-        const localname = urlStr.substr(ns.length)
-        if (localname.match(/^[a-zA-Z]+$/))
-          return prefix === ''
-          ? localname
-          : prefix + ':' + localname
-      }
-    }
-    if (new URL(node.href).host === new URL(urlStr).host) // https://github.com/stevenvachon/relateurl/issues/28 
-      return Relateurl.relate(node.href, urlStr, { output: Relateurl.ROOT_PATH_RELATIVE })
-    return urlStr
-  }
-}
-
-function noHash (url) {
-  const u = url.href
-  return new URL(u.substr(0, u.length - url.hash.length))
-}
-
-// a stand-alone and a split out ShapeTree
+ShapeTree.cache = {} // allow ShapeTree to load resources once.
 const Tests = [
+  // stand-alone ShapeTree
   new URL('solidApps/staticRoot/gh-flat/gh-flat-ShapeTree#org', Base),
-//  new URL('solidApps/staticRoot/gh-flat/gh-flat-ShapeTree-split-org#org', Base),
+  // same ShapeTree split into two chunks
+  new URL('solidApps/staticRoot/gh-flat/gh-flat-ShapeTree-split-org#org', Base),
 ]
 Promise.all(Tests.map(
   u => test(u)
